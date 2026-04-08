f4088c034673a94eb39da480f6efb807de801574
Nicolas Le Berre <nleberre5@gmail.com>
2026-04-08 12:40:40 +0200

    Refactor Firebase configuration to use environment variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,24 +2,23 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
- 
+
 // Configuration Firebase depuis les variables d'environnement
 const firebaseConfig = {
-  apiKey: "AIzaSyAtaiiMAgW1FiKAsQv8eqzam-8Rb1lXTEE",
-  authDomain: "menusemaine-dad56.firebaseapp.com",
-  projectId: "menusemaine-dad56",
-  storageBucket: "menusemaine-dad56.firebasestorage.app",
-  messagingSenderId: "784460574046",
-  appId: "1:784460574046:web:f82c23a11667cb1f1933cf",
-  measurementId: "G-F74KRRYR0S"
-};
- 
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+}
+
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig)
- 
+
 // Export des services Firebase
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
- 
+
 export default app
